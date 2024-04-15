f7c4bac6a26ac4431b26fc1b09e8de0261536049
keraberas <46618649+keraberase@users.noreply.github.com>
2024-04-15 13:27:37 +0300

    sart project

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+import { Feedback } from "../Feedback/Feedback";
+import { Options } from "../Options/Options";
+import { Notification } from "../Notifications/Notifications";
+import { Description } from "../Description/Description.jsx";
+
+export const App = () => {
+  const [feedback, setFeedback] = useState(() => {
+    const storedFeedback = JSON.parse(localStorage.getItem("feedback"));
+    return storedFeedback || { good: 0, neutral: 0, bad: 0 };
+  });
+
+  useEffect(() => {
+    localStorage.setItem("feedback", JSON.stringify(feedback));
+  }, [feedback]);
+
+  const updateFeedback = (feedbackType) => {
+    setFeedback((prevFeedback) => ({
+      ...prevFeedback,
+      [feedbackType]: prevFeedback[feedbackType] + 1,
+    }));
+  };
+
+  const resetFeedback = () => {
+    setFeedback({ good: 0, neutral: 0, bad: 0 });
+  };
+
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+
+  return (
+    <div>
+      <Description />
+      <Options
+        updateFeedback={updateFeedback}
+        resetFeedback={resetFeedback}
+        totalFeedback={totalFeedback}
+      />
+      {totalFeedback > 0 ? (
+        <Feedback feedback={feedback} totalFeedback={totalFeedback} />
+      ) : (
+        <Notification message="No feedback given yet." />
+      )}
+    </div>
+  );
+};
